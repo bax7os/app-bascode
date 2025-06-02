d36a3947192cd6e5847f9e0e8c5d134ad5dec1f8
Ana Clara <102690664+bax7os@users.noreply.github.com>
2025-06-02 15:14:07 -0400

    .

--- main.go
+++ main.go
@@ -16,6 +16,7 @@ func main() {
 	cookies.Configurar()
 	utils.CarregarTemplates()
 	r := router.Gerar()
+
 	fmt.Printf("Escutando na porta %d", config.Porta)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", config.Porta), r))
 
